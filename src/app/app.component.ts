f9a106eabab2e5669de73ee76223047d22499374
Gergely Szerovay <gergely@szerovay.hu>
2023-10-16 19:35:35 +0200

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  isChecked = signal(true);
+  items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' }
+  ];
+  radioValue = signal(1);
+}
